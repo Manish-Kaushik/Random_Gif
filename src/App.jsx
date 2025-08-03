9b8b3270e053a3fa40c3b9edf39a85164ea10602
ManishKaushik <boxofmanishkaushik@gmail.com>
2025-08-03 14:36:43 +0530

    Removed debug console log

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import Random from "./components/Random";
 // import Tag from "./components/Tag";
 import { Toast,ToastContainer } from "react-toastify";
+import { useEffect } from "react";
 export default function App() {
+  useEffect(() => {
+console.log("API KEY:", process.env.REACT_APP_GIPHY_API_KEY);
+}, []);
   return (
     
     <div>
